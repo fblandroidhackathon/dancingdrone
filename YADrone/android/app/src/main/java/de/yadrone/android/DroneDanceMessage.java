eb4e506b3cf9d9e741b437eb53548f60843d9edd
Pasquale Anatriello <Pasquale.Anatriello@digitaslbi.com>
2014-11-15 13:41:53 +0000

    Reintroduced builder

--- YADrone/android/app/src/main/java/de/yadrone/android/DroneDanceMessage.java
+++ YADrone/android/app/src/main/java/de/yadrone/android/DroneDanceMessage.java
@@ -11,6 +11,14 @@ public class DroneDanceMessage {
     private long startTimestamp;
     private long endTimestamp;
 
+    private DroneDanceMessage(Builder builder) {
+        setMidAmplitude(builder.midAmplitude);
+        setHighAmplitude(builder.highAmplitude);
+        setLowAmplitude(builder.lowAmplitude);
+        setStartTimestamp(builder.startTimestamp);
+        setEndTimestamp(builder.endTimestamp);
+    }
+
 
     public int getHighAmplitude() {
         return highAmplitude;
@@ -50,5 +58,45 @@ public class DroneDanceMessage {
 
     public void setMidAmplitude(int midAmplitude) {
         this.midAmplitude = midAmplitude;
+    }
+
+    public static final class Builder {
+        private int midAmplitude;
+        private int highAmplitude;
+        private int lowAmplitude;
+        private long startTimestamp;
+        private long endTimestamp;
+
+        public Builder() {
+        }
+
+        public Builder midAmplitude(int midAmplitude) {
+            this.midAmplitude = midAmplitude;
+            return this;
+        }
+
+        public Builder highAmplitude(int highAmplitude) {
+            this.highAmplitude = highAmplitude;
+            return this;
+        }
+
+        public Builder lowAmplitude(int lowAmplitude) {
+            this.lowAmplitude = lowAmplitude;
+            return this;
+        }
+
+        public Builder startTimestamp(long startTimestamp) {
+            this.startTimestamp = startTimestamp;
+            return this;
+        }
+
+        public Builder endTimestamp(long endTimestamp) {
+            this.endTimestamp = endTimestamp;
+            return this;
+        }
+
+        public DroneDanceMessage build() {
+            return new DroneDanceMessage(this);
+        }
     }
 }

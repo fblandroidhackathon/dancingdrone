a5115a2803b534cf6abf08f5c8cdd0d5f55d3a62
Pasquale Anatriello <Pasquale.Anatriello@digitaslbi.com>
2014-11-15 13:38:03 +0000

    Drone command thread

--- YADrone/android/app/src/main/java/de/yadrone/android/DroneHandler.java
+++ YADrone/android/app/src/main/java/de/yadrone/android/DroneHandler.java
@@ -2,6 +2,7 @@ package de.yadrone.android;
 
 import android.content.Context;
 import android.os.Handler;
+import android.os.Looper;
 import android.os.Message;
 
 import de.yadrone.base.IARDrone;
@@ -10,7 +11,8 @@ public class DroneHandler extends Handler {
 
     final IARDrone drone;
 
-    public DroneHandler(Context context) {
+    public DroneHandler(Context context, Looper looper) {
+        super(looper);
         YADroneApplication app = (YADroneApplication) context.getApplicationContext();
         drone = app.getARDrone();
     }

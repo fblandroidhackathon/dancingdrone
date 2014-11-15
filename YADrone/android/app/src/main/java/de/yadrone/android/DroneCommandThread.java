266db768cf58c1fba8efc96dbeda560cc3fff497
Pasquale Anatriello <Pasquale.Anatriello@digitaslbi.com>
2014-11-15 12:38:54 +0000

    drone command thread

---
+++ YADrone/android/app/src/main/java/de/yadrone/android/DroneCommandThread.java
@@ -0,0 +1,7 @@
+package de.yadrone.android;
+
+/**
+ * Created by anatriep on 15/11/2014.
+ */
+public class DroneCommandThread extends Thread{
+}
---
+++ YADrone/android/app/src/main/java/de/yadrone/android/DroneHandler.java
@@ -0,0 +1,36 @@
+package de.yadrone.android;
+
+import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
+
+import de.yadrone.base.IARDrone;
+
+public class DroneHandler extends Handler {
+
+    final IARDrone drone;
+
+    public DroneHandler(Context context) {
+        YADroneApplication app = (YADroneApplication) context.getApplicationContext();
+        drone = app.getARDrone();
+    }
+
+    @Override
+    public void handleMessage(Message msg) {
+        super.handleMessage(msg);
+        DroneDanceMessage message = (DroneDanceMessage) msg.obj;
+        long duration = message.getEndTimestamp() - message.getStartTimestamp();
+        drone.getCommandManager().goLeft(message.getHighAmplitude());
+        try {
+            Thread.sleep(duration / 2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        drone.getCommandManager().goRight(message.getHighAmplitude());
+        try {
+            Thread.sleep(duration / 2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
